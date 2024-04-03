b0243c954ab4b73ece79f455d3786f9ecaa6c1ac
DavidCuturilo <david.cuturilo@blinking.id>
2024-04-04 00:03:42 +0200

    fix: package.json updated - missed file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
+import { configService } from './config/config.service';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  await app.listen(configService.getPort());
+  logger.log(`Application is running on port: ${configService.getPort()}`);
 }
 bootstrap();
